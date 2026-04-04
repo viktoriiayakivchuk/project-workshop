a6655e75182dd64fe3d5bffe4701bea6630e4653
ViktoriiaYakivchuk <viktoriia.yakivchuk.20@pnu.edu.ua>
2026-04-04 14:43:45 +0300

    Add Lab 7: Azure Storage management with Terraform

--- main.tf
+++ main.tf
@@ -1,277 +1,101 @@
+# Task 1
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0" 
+      version = ">= 4.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = "d2a5b04c-fb08-4810-9540-2c26709beb16"
 }
 
-resource "azurerm_resource_group" "rg6" {
-  name     = "az104-rg6"
-  location = "North Europe" # Змінено для кращої доступності SKU
+resource "azurerm_resource_group" "rg7" {
+  name     = "az104-rg7"
+  location = "East US"
 }
 
-# Task 1: Provision an infrastructure - Virtual Network
-resource "azurerm_virtual_network" "vnet1" {
-  name                = "az104-06-vnet1"
-  address_space       = ["10.60.0.0/22"]
-  location            = azurerm_resource_group.rg6.location
-  resource_group_name = azurerm_resource_group.rg6.name
+resource "random_id" "server" {
+  byte_length = 8
 }
 
-resource "azurerm_subnet" "subnet0" {
-  name                 = "Subnet0"
-  resource_group_name  = azurerm_resource_group.rg6.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = ["10.60.0.0/24"]
-}
+resource "azurerm_storage_account" "storage" {
+  name                          = "staz104${lower(random_id.server.hex)}"
+  resource_group_name           = azurerm_resource_group.rg7.name
+  location                      = azurerm_resource_group.rg7.location
+  account_tier                  = "Standard"
+  account_replication_type      = "GRS"
+  public_network_access_enabled = true 
 
-resource "azurerm_subnet" "subnet1" {
-  name                 = "Subnet1"
-  resource_group_name  = azurerm_resource_group.rg6.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = ["10.60.1.0/24"]
-}
-
-# Task 1: Provision an infrastructure - Virtual Machines (Count: 2)
-resource "azurerm_network_interface" "nic" {
-  count               = 2 # Обмежено до 2 для дотримання квоти ядер
-  name                = "az104-06-nic${count.index}"
-  location            = azurerm_resource_group.rg6.location
-  resource_group_name = azurerm_resource_group.rg6.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = count.index == 0 ? azurerm_subnet.subnet0.id : azurerm_subnet.subnet1.id
-    private_ip_address_allocation = "Dynamic"
+  blob_properties {
+    versioning_enabled = true
   }
 }
 
-resource "azurerm_windows_virtual_machine" "vm" {
-  count               = 2
-  name                = "az104-06-vm${count.index}"
-  resource_group_name = azurerm_resource_group.rg6.name
-  location            = azurerm_resource_group.rg6.location
-  size                = "Standard_D2s_v3" 
-  admin_username      = "student"
-  admin_password      = "Pa55w.rd1234"
-  
-  custom_data = base64encode(<<-EOF
-    <powershell>
-    Install-WindowsFeature -name Web-Server -IncludeManagementTools
-    Remove-Item C:\inetpub\wwwroot\iisstart.htm
-    Add-Content -Path C:\inetpub\wwwroot\iisstart.htm -Value "Hello World from $env:computername"
-    </powershell>
-    EOF
-  )
+# Об'єднаний блок правил для Task 1 та Task 3
+resource "azurerm_storage_account_network_rules" "network_rules" {
+  storage_account_id = azurerm_storage_account.storage.id
 
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2022-Datacenter"
-    version   = "latest"
-  }
+  default_action             = "Deny"
+  # Ми прибрали ip_rules, щоб залишити доступ тільки для VNet згідно з Task 3
+  virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
+  bypass                     = ["AzureServices"]
 }
 
-# Task 2: Configure an Azure Load Balancer - Public IP
-resource "azurerm_public_ip" "lb_pip" {
-  name                = "az104-lbpip"
-  location            = azurerm_resource_group.rg6.location
-  resource_group_name = azurerm_resource_group.rg6.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
+resource "azurerm_storage_management_policy" "lifecycle" {
+  storage_account_id = azurerm_storage_account.storage.id
 
-# Task 2: Configure an Azure Load Balancer - Create LB
-resource "azurerm_lb" "lb" {
-  name                = "az104-lb"
-  location            = azurerm_resource_group.rg6.location
-  resource_group_name = azurerm_resource_group.rg6.name
-  sku                 = "Standard"
-
-  frontend_ip_configuration {
-    name                 = "az104-fe"
-    public_ip_address_id = azurerm_public_ip.lb_pip.id
-  }
-}
-
-# Task 2: Configure an Azure Load Balancer - Backend Pool
-resource "azurerm_lb_backend_address_pool" "be_pool" {
-  loadbalancer_id = azurerm_lb.lb.id
-  name            = "az104-be"
-}
-
-resource "azurerm_network_interface_backend_address_pool_association" "nic_assoc" {
-  count                   = 2
-  network_interface_id    = azurerm_network_interface.nic[count.index].id
-  ip_configuration_name   = "internal"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.be_pool.id
-}
-
-# Task 2: Configure an Azure Load Balancer - Health Probe
-resource "azurerm_lb_probe" "hp" {
-  loadbalancer_id = azurerm_lb.lb.id
-  name            = "az104-hp"
-  port            = 80
-  protocol        = "Tcp"
-  interval_in_seconds = 5
-}
-
-# Task 2: Configure an Azure Load Balancer - Load Balancing Rule
-resource "azurerm_lb_rule" "lb_rule" {
-  loadbalancer_id                = azurerm_lb.lb.id
-  name                           = "az104-lbrule"
-  protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
-  frontend_ip_configuration_name = "az104-fe"
-  backend_address_pool_ids        = [azurerm_lb_backend_address_pool.be_pool.id]
-  probe_id                       = azurerm_lb_probe.hp.id
-}
-
-# Вивід IP-адреси для тестування
-output "load_balancer_public_ip" {
-  value = azurerm_public_ip.lb_pip.ip_address
-}
-# Task 2: Security - Create NSG to allow HTTP
-resource "azurerm_network_security_group" "nsg" {
-  name                = "az104-06-nsg"
-  location            = azurerm_resource_group.rg6.location
-  resource_group_name = azurerm_resource_group.rg6.name
-
-  security_rule {
-    name                       = "allow-http"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-# Прив'язка NSG до обох підмереж (щоб пустити трафік до машин)
-resource "azurerm_subnet_network_security_group_association" "assoc0" {
-  subnet_id                 = azurerm_subnet.subnet0.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_subnet_network_security_group_association" "assoc1" {
-  subnet_id                 = azurerm_subnet.subnet1.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# Task 3: Dedicated Subnet for Application Gateway
-resource "azurerm_subnet" "subnet_agw" {
-  name                 = "Subnet-AGW"
-  resource_group_name  = azurerm_resource_group.rg6.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = ["10.60.3.0/24"]
-}
-
-# Task 3: Public IP for Application Gateway
-resource "azurerm_public_ip" "agw_pip" {
-  name                = "az104-06-pip-agw"
-  location            = azurerm_resource_group.rg6.location
-  resource_group_name = azurerm_resource_group.rg6.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# Task 3: Path-based Routing Configuration
-resource "azurerm_application_gateway" "app_gw" {
-  name                = "az104-06-appgw"
-  resource_group_name = azurerm_resource_group.rg6.name
-  location            = azurerm_resource_group.rg6.location
-
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 2
-  }
-
-  gateway_ip_configuration {
-    name      = "agw-ip-config"
-    subnet_id = azurerm_subnet.subnet_agw.id
-  }
-
-  frontend_port {
-    name = "http-port"
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = "agw-fe-config"
-    public_ip_address_id = azurerm_public_ip.agw_pip.id
-  }
-
-  # Пул для зображень (використовуємо vm0)
-  backend_address_pool {
-    name         = "az104-imagebe"
-    ip_addresses = [azurerm_network_interface.nic[0].private_ip_address]
-  }
-
-  # Пул для відео (використовуємо vm1)
-  backend_address_pool {
-    name         = "az104-videobe"
-    ip_addresses = [azurerm_network_interface.nic[1].private_ip_address]
-  }
-
-  backend_http_settings {
-    name                  = "az104-http"
-    cookie_based_affinity = "Disabled"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 60
-  }
-
-  http_listener {
-    name                           = "agw-listener"
-    frontend_ip_configuration_name = "agw-fe-config"
-    frontend_port_name             = "http-port"
-    protocol                       = "Http"
-  }
-
-  # ОСНОВНЕ: Правило маршрутизації за шляхом
-  request_routing_rule {
-    name               = "path-routing-rule"
-    rule_type          = "PathBasedRouting" 
-    http_listener_name = "agw-listener"
-    url_path_map_name  = "url-path-map"
-    priority           = 10
-  }
-
-  url_path_map {
-    name                               = "url-path-map"
-    default_backend_address_pool_name  = "az104-imagebe"
-    default_backend_http_settings_name = "az104-http"
-
-    path_rule {
-      name                       = "imagesBackend"
-      paths                      = ["/image/*"]
-      backend_address_pool_name  = "az104-imagebe"
-      backend_http_settings_name = "az104-http"
+  rule {
+    name    = "Movetocool"
+    enabled = true
+    filters {
+      blob_types = ["blockBlob"]
     }
-
-    path_rule {
-      name                       = "videosBackend"
-      paths                      = ["/video/*"]
-      backend_address_pool_name  = "az104-videobe"
-      backend_http_settings_name = "az104-http"
+    actions {
+      base_blob {
+        tier_to_cool_after_days_since_modification_greater_than = 30
+      }
     }
   }
+}
+
+# Task 2
+resource "azurerm_storage_container" "data" {
+  name                  = "data"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container_immutability_policy" "retention" {
+  storage_container_resource_manager_id = azurerm_storage_container.data.resource_manager_id
+  immutability_period_in_days           = 180
+}
+
+# Task 3: Manage Azure Files
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet1"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg7.location
+  resource_group_name = azurerm_resource_group.rg7.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg7.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+}
+
+resource "azurerm_storage_share" "share" {
+  name                 = "share1"
+  storage_account_name = azurerm_storage_account.storage.name
+  quota                = 1
 }
