aa9a379ce9e4c491954495e5ce8b38150818b23d
ViktoriiaYakivchuk <viktoriia.yakivchuk.20@pnu.edu.ua>
2026-04-16 14:10:43 +0300

    Lab 7

--- main.tf
+++ main.tf
@@ -1,22 +1,4 @@
 # Task 1
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = "d2a5b04c-fb08-4810-9540-2c26709beb16"
-}
-
 resource "azurerm_resource_group" "rg7" {
   name     = "az104-rg7"
   location = "East US"
